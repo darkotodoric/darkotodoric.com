751609a190abfc7e5b4574712ada012cf1c21a13
Darko Todoric <darko.todoric013@gmail.com>
2024-06-05 19:22:31 +0200

    Fix message margin

--- app/components/message.js
+++ app/components/message.js
@@ -4,7 +4,7 @@ import {motion} from "framer-motion";
 export default function Message() {
     return (
         <motion.div
-            className="w-3/4 my-10 md:my-14 lg:my-16 text-3xl md:text-4xl lg:text-5xl text-center"
+            className="w-3/4 my-12 md:my-14 text-4xl md:text-5xl text-center"
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.3 }}
